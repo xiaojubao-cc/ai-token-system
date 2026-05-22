3aed99c29a35d6d5b0aea5f5ecddd7fec90b63be
wangjianchao <wangjianchao@e-byte.com>
2026-05-22 11:56:07 +0800

    feat: 修改测试类

--- src/test/java/com/ai/system/AiTokenSystemApplicationTest.java
+++ src/test/java/com/ai/system/AiTokenSystemApplicationTest.java
@@ -1,7 +1,6 @@
 package com.ai.system;
 
 import com.ai.system.config.properties.TyyProperties;
-import com.ai.system.service.MailService;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,9 +11,6 @@ public class AiTokenSystemApplicationTest {
     @Resource
     private TyyProperties tyyProperties;
 
-    @Resource
-    private MailService mailService;
-
     @Test
     public void contextLoads() {
         System.out.println(tyyProperties.getAccessKey());
@@ -22,7 +18,6 @@ public class AiTokenSystemApplicationTest {
 
     @Test
     public void sendTestMail() {
-        mailService.sendTestMail("1171264943@qq.com");
-        System.out.println("测试邮件发送成功");
+
     }
 }
